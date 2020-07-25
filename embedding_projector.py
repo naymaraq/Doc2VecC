98350d9891509edfbbeefde9c509deeb1598517f
Davit <davitkar98@gmail.com>
2020-07-25 12:42:59 +0400

    init repo

---
+++ embedding_projector.py
@@ -0,0 +1,48 @@
+import json
+import os
+
+import numpy as np
+import tensorflow as tf
+from tensorboard.plugins import projector
+
+
+def project(word_vecs, id2word):
+    log_dir = 'logs'
+    if not os.path.exists(log_dir):
+        os.mkdir(log_dir)
+
+    with open(os.path.join(log_dir, 'metadata.tsv'), "w") as f:
+        f.write("{}\t{}\n".format("index", "word"))
+        for i, word in id2word.items():
+            f.write("{}\t{}\n".format(i, word))
+
+    weights = tf.Variable(word_vecs)
+    checkpoint = tf.train.Checkpoint(embedding=weights)
+    checkpoint.save(os.path.join(log_dir, "embedding.ckpt"))
+
+    # Set up config
+    config = projector.ProjectorConfig()
+    embedding = config.embeddings.add()
+
+    # The name of the tensor will be suffixed by `/.ATTRIBUTES/VARIABLE_VALUE`
+    embedding.tensor_name = "embedding/.ATTRIBUTES/VARIABLE_VALUE"
+    embedding.metadata_path = 'metadata.tsv'
+    projector.visualize_embeddings(log_dir, config)
+
+
+def normalize_rows(x):
+    """ Row normalization function
+
+    Implement a function that normalizes each row of a matrix to have
+    unit length.
+    """
+    N = x.shape[0]
+    x /= np.sqrt(np.sum(x ** 2, axis=1)).reshape((N, 1)) + 1e-30
+    return x
+
+
+if __name__ == "__main__":
+    id2word = json.load(open('embeddings/index2word.json', 'r'))
+    word_vecs = np.load("embeddings/word_vectors.npy", allow_pickle=True)
+    word_vecs = normalize_rows(word_vecs)
+    project(word_vecs, id2word)
---
+++ nearest_neighbors.py
@@ -0,0 +1,112 @@
+import argparse
+import json
+import os
+
+import numpy as np
+
+
+def normalize_rows(x):
+    """ Row normalization function
+
+    Implement a function that normalizes each row of a matrix to have
+    unit length.
+    """
+    N = x.shape[0]
+    x /= np.sqrt(np.sum(x ** 2, axis=1)).reshape((N, 1)) + 1e-30
+    return x
+
+
+class NN:
+    def nn_search(self, word):
+        pass
+
+    def load_vectors(self):
+        self.db = np.load(self.vectors_path, allow_pickle=True)
+        self.db = normalize_rows(self.db)
+
+    def load_vocab(self):
+        self.id2token = json.load(open(self.vocab_path, 'r'))
+        self.id2token = {int(i): w for i, w in self.id2token.items()}
+        self.token2id = {w: i for i, w in self.id2token.items()}
+
+    def get_query_vec(self, token):
+        ix = self.token2id[token]
+        return self.db[ix].reshape(1, -1)
+
+
+class NaiveNN(NN):
+
+    def __init__(self, topk, embedding_folder='./embeddings'):
+        self.topk = topk
+        self.vectors_path = os.path.join(embedding_folder, "word_vectors.npy")
+        self.vocab_path = os.path.join(embedding_folder, "index2word.json")
+
+        self.load_vocab()
+        self.load_vectors()
+
+    def nn_search(self, q_token):
+        cosine = lambda a, db: np.dot(a, self.db.T)
+        vec = self.get_query_vec(q_token)
+        scores = cosine(vec, self.db)[0]
+        indecies = scores.argsort()[-self.topk:][::-1]
+        D = [self.id2token[i] for i in indecies]
+        I = [scores[i] for i in indecies]
+        I = np.squeeze(I)
+        D = np.squeeze(D)
+        return D, I
+
+
+class AproximateNN(NN):
+
+    def __init__(self, topk, embedding_folder='./embeddings'):
+        self.topk = topk
+        self.vectors_path = os.path.join(embedding_folder, "word_vectors.npy")
+        self.vocab_path = os.path.join(embedding_folder, "index2word.json")
+
+        self.load_vectors()
+        self.load_vocab()
+
+    def load_vectors(self):
+        self.db = np.load(self.vectors_path, allow_pickle=True)
+        self.db = normalize_rows(self.db)
+        self.dim = self.db.shape[1]
+        self.index = faiss.IndexFlatIP(self.dim)
+        self.db = np.ascontiguousarray(self.db, dtype=np.float32)
+        self.index.add(self.db)
+
+    def nn_search(self, q_token):
+        vec = self.get_query_vec(q_token)
+        I, D = self.index.search(vec.reshape(1, -1), self.topk)
+        I = np.squeeze(I)
+        D = np.squeeze(D)
+        D = [self.id2token[i] for i in D]
+        return D, I
+
+
+nn = NaiveNN
+try:
+    import faiss
+
+    nn = AproximateNN
+except:
+    pass
+
+parser = argparse.ArgumentParser(description='NN Query')
+
+parser.add_argument('-topk', type=int, default=5)
+parser.add_argument('-word', type=str)
+
+args = parser.parse_args()
+
+if __name__ == "__main__":
+
+    nn_obj = nn(args.topk + 1)
+    most_similars, scores = nn_obj.nn_search(args.word)
+
+    i = 0
+    for k, v in zip(most_similars, scores):
+        if i == 0:
+            i += 1
+            continue
+        print('Top {} nearest: {}, score {}'.format(i, k, round(v, 4)))
+        i += 1
